0e9657b1b8069cd542627aee37d7125868729068
Dheeraj <dhirajyadav135@gmail.com>
2019-06-11 17:32:03 +0530

    Add cups-driverd and cups-deviced code.

---
+++ server/framework-config.h
@@ -0,0 +1,3 @@
+#define DRIVERBIN "/home/dj/Printer-Applications-Framework/share/driver"
+#define DATADIR "/home/dj/Printer-Applications-Framework/share"
+#define CACHEDIR "/home/dj/Printer-Applications-Framework/var/cache"
