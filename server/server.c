fcd0439e9bd86865ed0f51481359e4c845f77d99
Dheeraj <dhirajyadav135@gmail.com>
2019-08-23 22:26:43 +0530

    Add Support for Configuration File.

--- server/server.c
+++ server/server.c
@@ -91,6 +91,84 @@ void* start_avahi_monitor(void *n)
 }
 #endif
 
+void initialize()
+{
+  char filename[PATH_MAX];
+  snprintf(filename,PATH_MAX-1,"%s/config/framework.config",tmpdir);
+  cups_file_t* config = cupsFileOpen(filename,"r");
+  if(config==NULL)
+  {
+    fprintf(stderr,"Unable to open configuration file!\n");
+    return;
+  }
+  char line[2048];
+  int maxLines = 20;
+  char lines[maxLines][2][1024];
+  int numLines = 0;
+  while(cupsFileGets(config,line,sizeof(line)))
+  {
+    int len = strlen(line);
+    int comment = 0;
+    for(int i=0;i<len;i++)
+    {
+      if(isalnum(line[i])) break;
+      if(line[i]=='#'){
+        comment=1;
+        break;
+      }
+    }
+    // fprintf(stderr,"Line: %s %d\n",line,comment);
+    if(comment) continue;
+    char tokens[2][1024];
+    char temp[1024];
+    int index=0;
+    int numTokens=0;
+    for(int i=0;i<=len;i++)
+    {
+      if(isalpha(line[i])&&index<1024)
+      {
+        temp[index++]=line[i];
+      }
+      else{
+        if(index)
+        {
+          temp[index]='\0';
+          // fprintf(stderr,"token: %s %d\n",temp,numTokens);
+          strcpy(tokens[numTokens++],temp);
+          index=0;
+          if(numTokens==2) break;
+        }
+      }
+    }
+    if(numTokens==2)
+    {
+      strcpy(lines[numLines][0],tokens[0]);
+      strcpy(lines[numLines][1],tokens[1]);
+      numLines++;
+      if(numLines>=maxLines)
+        break;
+    }
+  }
+  cupsFileClose(config);
+  for(int i=0;i<numLines;i++)
+  {
+    // fprintf(stderr,"Lines: %d %s %s\n",i,lines[i][0],lines[i][1]);
+    if(!strcmp(lines[i][0],"DebuggingLevel"))
+    {
+      char level[2] = "1";
+      if(!strncasecmp(lines[i][1],"DEBUG",5))
+      {
+        level[0] = '2';
+      }
+      else if(!strncasecmp(lines[i][1],"DEBUG2",6))
+      {
+        level[0] = '3';
+      }
+      setenv("DEBUG_LEVEL",level,1);
+    }
+  }
+}
+
 /*
  * main() -
  */
@@ -110,6 +188,8 @@ int main(int argc,char* argv[])
   if(getenv("SNAP_COMMON"))
     tmpdir = strdup(getenv("SNAP_COMMON"));
   else tmpdir = strdup("/var/tmp");
+
+  initialize();
   
   con_devices = cupsArrayNew((cups_array_func_t)compare_devices,NULL);
   temp_devices = cupsArrayNew((cups_array_func_t)compare_devices,NULL);
@@ -238,7 +318,7 @@ get_devices(int insert,int signal)
       *cj =0;
       cj =0;
     }
-    debug_printf("Signal: %s\n",includes);
+    debug_printf("DEBUG: Signal: %s\n",includes);
     strcpy(name,"deviced");
     strcpy(reques_id,DEVICED_REQ);
     strcpy(limit,DEVICED_LIM);
